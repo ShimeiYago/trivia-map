643da96f18378ce978866d11d246ec3ad301b738
ShimeiYago <harrisonhightower4@gmail.com>
2022-04-12 18:04:24 +0900

    remove comment

--- src/store/markers/model/index.ts
+++ src/store/markers/model/index.ts
@@ -1,11 +1,6 @@
 import { LoadingState } from 'types/loading-state';
 import { Position } from 'types/position';
 
-// export type Marker = {
-//   postId: string;
-//   position: Position;
-//   title: string;
-// };
 export interface MarkerDict {
   [shopId: string]: {
     position: Position;

0508d667cb9801565ab1cfc09a8d08e0cbd5a226
ShimeiYago <harrisonhightower4@gmail.com>
2022-04-29 09:13:31 +0900

    Routing

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import { App } from 'views/pages/app';
 import { store } from 'store';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import { ErrorBoundary } from 'error-boundary';
 
@@ -13,11 +14,17 @@ process.env.NODE_ENV === 'production' && (console.log = () => {});
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </Provider>
+    <ErrorBoundary>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<App />} />
+            <Route path="/posts/:id" element={<div>Post Page (TODO)</div>} />
+            <Route path="*" element={<div>404 not found (TODO)</div>} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root'),
 );

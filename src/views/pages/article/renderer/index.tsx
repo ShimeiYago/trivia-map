6f00e0c17ed5b299f7b83310604931e0c277cd16
ShimeiYago <harrisonhightower4@gmail.com>
2022-05-01 16:37:02 +0900

    Fix article page layout

--- src/views/pages/article/renderer/index.tsx
+++ src/views/pages/article/renderer/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { Box, CircularProgress, Typography } from '@mui/material';
+import { Box, CircularProgress, Grid, Typography } from '@mui/material';
 import { LoadingState } from 'types/loading-state';
+import { GlobalMenu } from 'views/components/organisms/global-menu';
+import { wrapper } from '../styles';
+import { Link } from 'react-router-dom';
+import { ArticlePaper } from 'views/components/atoms/article-paper';
 
 export class Renderer extends React.Component<Props> {
   componentDidMount() {
@@ -20,14 +24,39 @@ export class Renderer extends React.Component<Props> {
     }
 
     return (
+      <Box sx={wrapper}>
+        <GlobalMenu barPosition="static" />
+        <Box sx={{ marginTop: 5, marginX: 10 }}>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <ArticlePaper variant="navi">
+                <Link to="/">マップへ戻る</Link>
+              </ArticlePaper>
+            </Grid>
+            <Grid item xs={8}>
+              <ArticlePaper variant="main">
+                {this.renderContents()}
+              </ArticlePaper>
+            </Grid>
+            <Grid item xs={4}>
+              <ArticlePaper variant="side">サイドバー</ArticlePaper>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    );
+  }
+
+  protected renderContents = () => {
+    return (
       <>
-        <Typography variant="h6" component="h2">
+        <Typography variant="h4" component="h2" align="center">
           {this.props.title}
         </Typography>
         <Typography sx={{ mt: 2 }}>{this.props.content}</Typography>
       </>
     );
-  }
+  };
 }
 
 export type Props = {

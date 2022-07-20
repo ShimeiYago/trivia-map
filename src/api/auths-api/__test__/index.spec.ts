5240dce84091c68b87925ae468b18a6eb8d170cd
ShimeiYago <harrisonhightower4@gmail.com>
2022-07-20 09:43:17 +0900

    Add reset-password api

--- src/api/auths-api/__test__/index.spec.ts
+++ src/api/auths-api/__test__/index.spec.ts
@@ -1,6 +1,7 @@
 import { ApiError } from 'api/utils/handle-axios-error';
 import { login } from '../login';
 import { registration } from '../registration';
+import { resetPassword } from '../reset-password';
 
 describe('login', () => {
   beforeEach(() => {
@@ -62,5 +63,34 @@ describe('registration', () => {
     await expect(
       registration('user@example.com', 'Axel', 'password1', 'password2'),
     ).rejects.toEqual(expectedApiError);
+  });
+});
+
+describe('reset-password', () => {
+  beforeEach(() => {
+    process.env.REACT_APP_MOCK = '';
+  });
+  afterEach(() => {
+    process.env.REACT_APP_MOCK = '';
+  });
+
+  it('handle nomal response', async () => {
+    process.env.REACT_APP_MOCK = 'normal';
+
+    const response = await resetPassword('user@example.com');
+    expect(response).toEqual(undefined);
+  });
+
+  it('handle error response', async () => {
+    process.env.REACT_APP_MOCK = 'error';
+
+    const expectedApiError: ApiError<unknown> = {
+      status: 500,
+      data: {},
+      errorMsg: 'Intentional API Error with mock',
+    };
+    await expect(resetPassword('user@example.com')).rejects.toEqual(
+      expectedApiError,
+    );
   });
 });
--- src/api/auths-api/reset-password.ts
+++ src/api/auths-api/reset-password.ts
@@ -3,41 +3,27 @@ import { AxiosError } from 'axios';
 import { handleAxiosError } from '../utils/handle-axios-error';
 import { getAxiosInstance } from 'api/utils/get-axios-instance';
 
-export async function registration(
-  nickname: string,
-  email: string,
-  password1: string,
-  password2: string,
-): Promise<void> {
+export async function resetPassword(email: string): Promise<void> {
   const axiosInstance = getAxiosInstance({}, {});
 
-  const requestData: RegistrationRequest = {
-    nickname: nickname,
+  const requestData: ResetPasswordRequest = {
     email: email,
-    password1: password1,
-    password2: password2,
   };
 
   try {
     // TODO: Set reasonable timeout
-    await axiosInstance.post(`${BASE_URL}/auths/registration`, requestData);
+    await axiosInstance.post(`${BASE_URL}/auths/password/reset`, requestData);
   } catch (error) {
     const axiosError = error as AxiosError;
     throw handleAxiosError<ValidationError>(axiosError);
   }
 }
 
-export type RegistrationRequest = {
-  nickname: string;
+export type ResetPasswordRequest = {
   email: string;
-  password1: string;
-  password2: string;
 };
 
 export type ValidationError = {
   email?: string[];
-  nickname?: string[];
-  password1?: string[];
-  password2?: string[];
   non_field_errors?: string[];
 };

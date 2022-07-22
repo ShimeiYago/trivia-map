3067778c63f1fcfbe2632b95cbfa643deb2e4649
ShimeiYago <harrisonhightower4@gmail.com>
2022-07-23 00:56:46 +0900

    Fix modal width

--- src/views/components/moleculars/box-modal/styles.ts
+++ src/views/components/moleculars/box-modal/styles.ts
@@ -5,9 +5,7 @@ export const boxStyle: SxProps = {
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
-  width: 400,
   bgcolor: 'background.paper',
-  border: '2px solid #000',
+  width: '350px',
   boxShadow: 24,
-  p: 4,
 };

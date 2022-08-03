288e556d9aa8e09f844b430e006e411e85ff7853
ShimeiYago <harrisonhightower4@gmail.com>
2022-08-03 15:52:18 +0900

    Redirect if edit postId is invalid

--- src/views/pages/map-page/index.tsx
+++ src/views/pages/map-page/index.tsx
@@ -6,10 +6,17 @@ import {
 } from 'store/article-form/selector';
 import { Renderer, Props } from './renderer';
 import { isMobile } from 'react-device-detect';
-import { useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
+import { NOT_FOUND_LINK } from 'constant/links';
 
 export function MapPage() {
   const { postId } = useParams();
+  const postIdNumber = Number(postId);
+
+  if (postId && !postIdNumber) {
+    return <Navigate to={NOT_FOUND_LINK} />;
+  }
+
   const articleFormId = useAppSelector(selectArticleFormId);
   const postIdToEdit = Number(postId) ?? articleFormId;
 

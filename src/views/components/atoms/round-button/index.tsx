c8972e164dd7c302cd5c9d5e93b2c3f6b0942f47
ShimeiYago <harrisonhightower4@gmail.com>
2022-08-30 01:00:08 +0900

    New atom component round-button

---
+++ src/views/components/atoms/round-button/index.tsx
@@ -0,0 +1,29 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+import styles from './index.module.css';
+
+export const RoundButton: React.FC<Props> = (props) => {
+  const classNames = [styles['round-button']];
+  if (props.selected) {
+    classNames.push(styles['selected']);
+  }
+
+  return (
+    <button className={`${classNames.join(' ')}`} onClick={props.onClick}>
+      <Typography
+        variant="button"
+        component="div"
+        color={props.selected ? 'white' : 'black'}
+      >
+        {props.children}
+      </Typography>
+    </button>
+  );
+};
+
+export type Props = {
+  children: React.ReactNode;
+  selected?: boolean;
+
+  onClick?: () => void;
+};

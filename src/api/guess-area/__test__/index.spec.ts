17b954ffeed17139e74fbb805cc92490b3ba6f57
ShimeiYago <harrisonhightower4@gmail.com>
2022-09-05 01:04:56 +0900

    Add guess-area api

---
+++ src/api/guess-area/__test__/index.spec.ts
@@ -0,0 +1,32 @@
+import { ApiError } from 'api/utils/handle-axios-error';
+import { guessArea } from '..';
+
+describe('getRemoteMarkers', () => {
+  beforeEach(() => {
+    process.env.REACT_APP_MOCK = '';
+  });
+
+  it('handle nomal response', async () => {
+    process.env.REACT_APP_MOCK = 'normal';
+
+    const response = await guessArea({ lat: 1, lng: 1, park: 'L' });
+    expect(response.areaNames).toEqual([
+      'シー',
+      'メディテレーニアンハーバー',
+      'ポルトパラディーゾ',
+    ]);
+  });
+
+  it('handle error response', async () => {
+    process.env.REACT_APP_MOCK = 'error';
+
+    const expectedApiError: ApiError<unknown> = {
+      status: 500,
+      data: {},
+      errorMsg: 'Intentional API Error with mock',
+    };
+    await expect(guessArea({ lat: 1, lng: 1, park: 'L' })).rejects.toEqual(
+      expectedApiError,
+    );
+  });
+});
---
+++ src/api/mock/guess-area-response/index.ts
@@ -0,0 +1,5 @@
+import { GuessAreaResponse } from '../../guess-area';
+
+export const mockGuessAreaResponse: GuessAreaResponse = {
+  areaNames: ['シー', 'メディテレーニアンハーバー', 'ポルトパラディーゾ'],
+};

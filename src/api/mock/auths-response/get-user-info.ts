a938f496c6ba0c59b88551df3062f0e17826c001
ShimeiYago <harrisonhightower4@gmail.com>
2022-09-12 21:53:58 +0900

    Change mock image url

--- src/api/mock/auths-response/get-user-info.ts
+++ src/api/mock/auths-response/get-user-info.ts
@@ -4,5 +4,5 @@ export const mockGetUserInfoResponse: User = {
   userId: 1,
   email: 'user@example.com',
   nickname: 'Axel',
-  icon: 'https://www.disneyparkstory.com/wp-content/uploads/2017/09/apple-touch-icon.png',
+  icon: 'https://disneyparkstory.com/wp-content/uploads/2017/09/apple-touch-icon.png',
 };

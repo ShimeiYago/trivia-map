63acb90d3a384b05aa3044399e8822c88dd887c7
ShimeiYago <harrisonhightower4@gmail.com>
2022-09-23 12:30:49 +0900

    Add inquiry api

---
+++ src/api/mock/inquiry-response/index.ts
@@ -0,0 +1,8 @@
+import { InquiryRequestAndResponse } from './../../inquiry-api/index';
+
+export const mockInquiryResponse: InquiryRequestAndResponse = {
+  email: 'xxx@xxx.com',
+  name: 'name',
+  tag: 'tag',
+  message: 'message',
+};

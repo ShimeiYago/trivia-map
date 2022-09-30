269ad787eab22f245713d643b9584833b529a180
ShimeiYago <harrisonhightower4@gmail.com>
2022-09-30 23:32:03 +0900

    Create common error component

--- src/views/pages/common-error-page/renderer/index.tsx
+++ src/views/pages/common-error-page/renderer/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
-import { MAP_PAGE_LINK } from 'constant/links';
-import { Link } from 'react-router-dom';
 import { GlobalMenu } from 'views/components/organisms/global-menu';
 import notFoundImage from 'images/404.png';
 import { HeadAppender } from 'helper-components/head-appender';
 import { pageTitleGenerator } from 'utils/page-title-generator';
 import { PAGE_NAMES } from 'constant/page-names';
+import { InternalError } from 'views/components/moleculars/internal-error';
 
 export const Renderer: React.FC<Props> = (props) => {
   switch (props.errorStatus) {
@@ -40,15 +39,7 @@ const render404Error = () => {
 const render500Error = () => {
   return (
     <HeadAppender title={pageTitleGenerator(PAGE_NAMES.internalError)}>
-      <Typography align="center" component="div" sx={{ mt: 3 }}>
-        <Typography component="h1" fontSize={18}>
-          エラーが発生しました。
-        </Typography>
-        <Typography fontSize={18}>しばらく時間を空けてからもう一度お試しください。</Typography>
-        <Typography component="h1" fontSize={20} sx={{ mt: 1 }}>
-          <Link to={MAP_PAGE_LINK}>トップへ戻る</Link>
-        </Typography>
-      </Typography>
+      <InternalError />
     </HeadAppender>
   );
 };

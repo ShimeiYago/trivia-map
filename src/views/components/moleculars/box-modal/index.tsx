101ed0ff02307cbd24bbd1c86ddd208b32355326
ShimeiYago <harrisonhightower4@gmail.com>
2022-10-06 13:52:10 +0900

    Do not close login modal when outside is clicked

--- src/views/components/moleculars/box-modal/index.tsx
+++ src/views/components/moleculars/box-modal/index.tsx
@@ -1,12 +1,27 @@
 import React, { ReactNode } from 'react';
-import { Modal, Box } from '@mui/material';
+import { Modal, Box, Typography, IconButton } from '@mui/material';
 import { boxStyle } from './styles';
+import CloseIcon from '@mui/icons-material/Close';
 
 export class BoxModal extends React.Component<Props> {
   render() {
+    const closeButton = (
+      <Typography align="right" color="gray">
+        <IconButton color="inherit" size="large" onClick={this.props.onClose}>
+          <CloseIcon />
+        </IconButton>
+      </Typography>
+    );
+
     return (
-      <Modal open={this.props.open} onClose={this.props.onClose}>
-        <Box sx={boxStyle}>{this.props.children}</Box>
+      <Modal
+        open={this.props.open}
+        onClose={this.props.showCloseButton ? undefined : this.props.onClose}
+      >
+        <Box sx={boxStyle}>
+          {this.props.showCloseButton && closeButton}
+          {this.props.children}
+        </Box>
       </Modal>
     );
   }
@@ -16,4 +31,5 @@ export type Props = {
   open: boolean;
   onClose?: () => void;
   children: ReactNode;
+  showCloseButton?: boolean;
 };

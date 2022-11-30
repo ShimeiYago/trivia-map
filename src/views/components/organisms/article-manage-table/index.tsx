b6afd617b88e26346779ccd5f0974d9a50495896
ShimeiYago <harrisonhightower4@gmail.com>
2022-12-01 01:20:01 +0900

    Open edit pages with same tab

--- src/views/components/organisms/article-manage-table/index.tsx
+++ src/views/components/organisms/article-manage-table/index.tsx
@@ -3,6 +3,7 @@ import { throwError } from 'store/global-error/slice';
 import { Renderer, Props } from './renderer';
 import { isMobile } from 'react-device-detect';
 import { fetchMarkers } from 'store/markers/actions';
+import { initialize } from 'store/article-form/actions';
 
 export function ArticleManageTable() {
   const dispatch = useAppDispatch();
@@ -11,6 +12,7 @@ export function ArticleManageTable() {
     throwError: (status: number) => dispatch(throwError(status)),
     isMobile: isMobile,
     fetchMarkers: () => dispatch(fetchMarkers()),
+    initialize: () => dispatch(initialize()),
   };
 
   return <Renderer {...props} />;

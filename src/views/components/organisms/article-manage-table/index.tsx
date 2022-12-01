a067a8f376db31f6bde886f5dc9454502902e9c3
ShimeiYago <harrisonhightower4@gmail.com>
2022-12-02 00:13:30 +0900

    Initial park for edit mode

--- src/views/components/organisms/article-manage-table/index.tsx
+++ src/views/components/organisms/article-manage-table/index.tsx
@@ -1,17 +1,20 @@
-import { useAppDispatch } from 'store';
+import { useAppDispatch, useAppSelector } from 'store';
 import { throwError } from 'store/global-error/slice';
 import { Renderer, Props } from './renderer';
 import { isMobile } from 'react-device-detect';
 import { fetchMarkers } from 'store/markers/actions';
 import { initialize } from 'store/article-form/actions';
+import { Park } from 'types/park';
+import { selectFocusingPark } from 'store/markers/selector';
 
 export function ArticleManageTable() {
   const dispatch = useAppDispatch();
 
   const props: Props = {
+    park: useAppSelector(selectFocusingPark),
     throwError: (status: number) => dispatch(throwError(status)),
     isMobile: isMobile,
-    fetchMarkers: () => dispatch(fetchMarkers()),
+    fetchMarkers: (park: Park) => dispatch(fetchMarkers(park)),
     initialize: () => dispatch(initialize()),
   };
 

ce5a1952ddc8dbf26884d6fd8e0727a3e5f80f0a
ShimeiYago <harrisonhightower4@gmail.com>
2022-12-09 00:14:11 +0900

    Add site url env

--- src/constant/head-tags.ts
+++ src/constant/head-tags.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_HEAD_TAGS = {
   description: 'ディズニーの隠れミッキーやストーリーなどの豆知識を地図上で自由に共有できるアプリ',
   ogType: 'website',
-  imageUrl: '/site-image.png',
+  imageName: 'site-image.png',
   ogLocale: 'ja_JP',
   twitterCard: 'summary_large_image',
   twitterSite: '@Axel_1864',

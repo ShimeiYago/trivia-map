1c002f9556f79b747a408ad10969160c5412e32e
ShimeiYago <harrisonhightower4@gmail.com>
2023-01-15 22:53:17 +0900

    Twitter login with new window

--- src/views/components/organisms/twitter-login-button/renderer/index.tsx
+++ src/views/components/organisms/twitter-login-button/renderer/index.tsx
@@ -4,11 +4,18 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import { twitterRequestToken } from 'api/auths-api/twitter-request-token';
 import { LoadingButton } from '@mui/lab';
 import { getDomain } from 'utils/get-domain.ts';
+import { TwitterAccessTokenResponse } from 'api/auths-api/twitter-access-token';
+import { twitterLogin } from 'api/auths-api/twitter-login';
+import { User } from 'types/user';
 
 export class Renderer extends React.Component<Props, State> {
   state: State = {
     loading: false,
   };
+
+  componentDidMount() {
+    window.addEventListener('message', this.handleMessage);
+  }
 
   render() {
     return (
@@ -18,6 +25,7 @@ export class Renderer extends React.Component<Props, State> {
         onClick={this.handleClick}
         loading={this.state.loading}
         fullWidth={true}
+        disabled={!!this.props.userInfo}
         sx={{ textTransform: 'none' }}
       >
         Twitterでログイン
@@ -25,9 +33,26 @@ export class Renderer extends React.Component<Props, State> {
     );
   }
 
-  protected handleClick = async () => {
-    const { throwError } = this.props;
+  protected handleMessage = async (event: MessageEvent<TwitterAccessTokenResponse>) => {
+    const twitterAccessTokenResponse = event.data;
 
+    try {
+      const loginResponse = await twitterLogin(twitterAccessTokenResponse);
+
+      this.props.loginSuccess(loginResponse.user);
+
+      this.props.setAccessTokenExpiration(new Date(loginResponse.access_token_expiration));
+      this.props.setRefreshTokenExpiration(new Date(loginResponse.refresh_token_expiration));
+
+      this.setState({
+        loading: false,
+      });
+    } catch (error) {
+      this.props.throwError(500);
+    }
+  };
+
+  protected handleClick = async () => {
     this.setState({
       loading: true,
     });
@@ -38,20 +63,19 @@ export class Renderer extends React.Component<Props, State> {
       const twitterRequestTokenResponse = await twitterRequestToken({
         callbackUrl: `${domain}${TWITTER_CALLBACK_LINK}`,
       });
-      this.props.redirectTo(twitterRequestTokenResponse.authenticateUrl);
+      window.open(twitterRequestTokenResponse.authenticateUrl, '_blank', 'width=640, height=480');
     } catch (error: unknown) {
-      throwError(500);
+      this.props.throwError(500);
     }
-
-    this.setState({
-      loading: false,
-    });
   };
 }
 
 export type Props = {
-  redirectTo: (url: string) => void;
+  userInfo?: User;
   throwError: (status: number) => void;
+  loginSuccess: (user: User) => void;
+  setAccessTokenExpiration: (expirationDate: Date) => void;
+  setRefreshTokenExpiration: (expirationDate: Date) => void;
 };
 
 export type State = {

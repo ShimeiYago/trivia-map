6d2f48a762c6eaa8f9c04099eaf2ca0976eecc96
ShimeiYago <harrisonhightower4@gmail.com>
2023-02-20 14:38:33 +0900

    update land tiles

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -87,7 +87,7 @@ export const UPLOAD_IMAGE_MAX_LENGTH = {
   icon: 200,
 };
 
-export const TDL_TILE_URL = '/tdl-map-tiles/{z}/{x}/{y}.png';
+export const TDL_TILE_URL = '/l-tiles/{z}/{x}/{y}.png';
 export const TDS_TILE_URL = '/tds-map-tiles/{z}/{x}/{y}.png';
 
 export const ZOOMS = {

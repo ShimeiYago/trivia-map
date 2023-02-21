f846a97e750e8aebcc0f4f72532d8692cca34382
shimei.yago <shimei.yago@rakuten.com>
2023-02-21 10:20:25 +0900

    Fix my articles table layout

--- src/api/articles-api/get-my-articles.ts
+++ src/api/articles-api/get-my-articles.ts
@@ -2,16 +2,13 @@ import { API_TIMEOUT, BASE_URL } from 'constant';
 import { AxiosError, AxiosResponse } from 'axios';
 import { handleAxiosError } from '../utils/handle-axios-error';
 import { getAxiosInstance } from 'api/utils/get-axios-instance';
-import { mockGetArticlesPreviewsResponse } from '../mock/articles-response';
+import { mockGetMyArticlesResponse } from '../mock/articles-response';
 import { PaginationResponse } from 'api/types/pagination-response';
 import { Park } from 'types/park';
 import { getUrlParameters } from 'utils/get-url-parameters';
 
 export async function getMyArticles(param: GetMyArticlesParam): Promise<GetMyArticlesResponse> {
-  const axiosInstance = getAxiosInstance(
-    { timeout: API_TIMEOUT.short },
-    mockGetArticlesPreviewsResponse,
-  );
+  const axiosInstance = getAxiosInstance({ timeout: API_TIMEOUT.short }, mockGetMyArticlesResponse);
 
   const urlParams = getUrlParameters(param);
   const url = `${BASE_URL}/articles/mine${urlParams}`;

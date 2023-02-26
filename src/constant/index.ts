a4763411cfa1faaff50209f441a89ff90970f9ad
ShimeiYago <harrisonhightower4@gmail.com>
2023-02-27 00:38:22 +0900

    replace tds map

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -88,7 +88,7 @@ export const UPLOAD_IMAGE_MAX_LENGTH = {
 };
 
 export const TDL_TILE_URL = '/l-tiles/{z}/{x}/{y}.png';
-export const TDS_TILE_URL = '/tds-map-tiles/{z}/{x}/{y}.png';
+export const TDS_TILE_URL = '/s-tiles/{z}/{x}/{y}.png';
 
 export const ZOOMS = {
   min: 1,

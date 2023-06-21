e40a72236c025314769dfe77ca7c7f49e0503520
ShimeiYago <harrisonhightower4@gmail.com>
2023-06-21 22:48:41 +0900

    Keep param of article manage table

--- src/views/components/organisms/article-manage-table/index.tsx
+++ src/views/components/organisms/article-manage-table/index.tsx
@@ -5,9 +5,25 @@ import { isMobile } from 'react-device-detect';
 import { initializeFetchingState } from 'store/markers/actions';
 import { initialize } from 'store/article-form/actions';
 import { updateUser } from 'store/auths/actions';
+import { useLocation } from 'react-router-dom';
+import { GetMyArticlesParam } from 'api/articles-api/get-my-articles';
+import { PARKS } from 'constant';
 
 export function ArticleManageTable() {
   const dispatch = useAppDispatch();
+
+  const search = useLocation().search;
+  const query = new URLSearchParams(search);
+
+  const parkParam = query.get('park');
+  const isDraftParam = query.get('isDraft');
+
+  const conditions: GetMyArticlesParam = {
+    category: query.get('category') ? Number(query.get('category')) : undefined,
+    park: parkParam === PARKS.land || parkParam === PARKS.sea ? parkParam : undefined,
+    page: query.get('page') ? Number(query.get('page')) : 1,
+    isDraft: isDraftParam === 'true' || isDraftParam === 'false' ? isDraftParam : undefined,
+  };
 
   const props: Props = {
     throwError: (status: number) => dispatch(throwError(status)),
@@ -15,6 +31,7 @@ export function ArticleManageTable() {
     initializeFetchingState: () => dispatch(initializeFetchingState()),
     initialize: () => dispatch(initialize()),
     refreshUser: () => dispatch(updateUser(undefined)),
+    initialSearchParam: conditions,
   };
 
   return <Renderer {...props} />;

9497b1cf3070217e66fc0645edecc1b28bc00684
ShimeiYago <harrisonhightower4@gmail.com>
2023-08-22 01:09:31 +0900

    Update special map interfaces

--- src/api/mock/special-map-response/index.ts
+++ src/api/mock/special-map-response/index.ts
@@ -10,6 +10,17 @@ export const mockGetSpecialMapResponse: GetSpecialMapResponse = {
   isPublic: true,
   description: '定番から穴場まで、ディズニーのトイレマップです。',
   selectablePark: 'both',
+  author: {
+    userId: 1,
+    nickname: 'Axel',
+    icon: 'https://disneyparkstory.com/wp-content/uploads/2017/09/apple-touch-icon.png',
+    url: 'https://disneyparkstory.com',
+  },
+  minLatitude: -255.0,
+  maxLatitude: 0.0,
+  minLongitude: 0.0,
+  maxLongitude: 255.0,
+  createdAt: '2023/1/1 12:00',
 };
 
 export const mockGetSpecialMapsResponseWithPagination: GetSpecialMapsResponseWithPagination = {
@@ -28,7 +39,6 @@ export const mockGetSpecialMapsResponseWithPagination: GetSpecialMapsResponseWit
       thumbnail: null,
       isPublic: true,
       description: 'パーク中の「関係者以外立ち入り禁止」の看板のまとめです。',
-      selectablePark: 'L',
     },
   ],
 };
--- src/api/special-map-api/get-special-maps.ts
+++ src/api/special-map-api/get-special-maps.ts
@@ -5,13 +5,12 @@ import { handleAxiosError } from '../utils/handle-axios-error';
 import { getAxiosInstance } from 'api/utils/get-axios-instance';
 import { mockGetSpecialMapsResponseWithPagination } from 'api/mock/special-map-response';
 import { getUrlParameters } from 'utils/get-url-parameters';
-import { GetSpecialMapResponse } from './get-special-map';
 
 export async function getSpecialMaps(param: {
   page?: number;
 }): Promise<GetSpecialMapsResponseWithPagination> {
   const urlParams = getUrlParameters(param);
-  const url = `${BASE_URL}/special-map/maps${urlParams}`;
+  const url = `${BASE_URL}/special-map/maps/public-previews${urlParams}`;
 
   const axiosInstance = getAxiosInstance({}, mockGetSpecialMapsResponseWithPagination);
 
@@ -24,4 +23,12 @@ export async function getSpecialMaps(param: {
   }
 }
 
-export type GetSpecialMapsResponseWithPagination = PaginationResponse<GetSpecialMapResponse>;
+export type GetSpecialMapPreviewResponse = {
+  specialMapId: number;
+  title: string;
+  thumbnail: string | null;
+  isPublic: boolean;
+  description: string;
+};
+
+export type GetSpecialMapsResponseWithPagination = PaginationResponse<GetSpecialMapPreviewResponse>;

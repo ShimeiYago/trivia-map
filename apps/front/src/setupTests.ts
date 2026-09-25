a935835ee9d736b8b2e8fefd15050bed3c5ceba1
ShimeiYago <harrisonhightower4@gmail.com>
2026-09-25 14:54:30 +0900

    ci: require lint in workspace verification

--- apps/front/src/setupTests.ts
+++ apps/front/src/setupTests.ts
@@ -9,4 +9,4 @@ import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { createSerializer } from 'enzyme-to-json';
 configure({ adapter: new Adapter() });
-expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }) as any);
